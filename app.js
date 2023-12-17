d0923dcd7edd90a60c98bf9c8c8198e55b52d2c5
janania <joseanania47@gmail.com>
2023-12-17 18:22:14 -0300

    commit 1

---
+++ app.js
@@ -0,0 +1,8 @@
+
+// creo una función que recibe un parámetro 
+const saludar = (nombre) => {
+    return `Saludos ${nombre}`;
+}
+
+// imprimo en consola la función, asignando el parámetro
+console.log(saludar( 'janania' ));
